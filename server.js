e118f8380ac494fdabc95ecdd86fb519517c751e
Cyrill SEMAH <cyrillsemah@Mac-001.lan>
2025-03-18 13:56:17 +0100

    Fix static files and port for Heroku

--- server.js
+++ server.js
@@ -71,8 +71,6 @@ app.get('/', (req, res) => {
 });
 
 // Démarrer le serveur avec le port dynamique Heroku
-const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
